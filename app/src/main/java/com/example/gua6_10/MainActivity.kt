1fbe294a2429fe81a7580f4b5161bc9fb98c5ab0
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2020-03-24 12:03:31 -0400

    guia 6 implementada

---
+++ app/src/main/java/com/example/gua6_10/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gua6_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gua6_10.toolbar as toolbar1
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gua6_10/toolbar.kt
@@ -0,0 +1,14 @@
+package com.example.gua6_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class toolbar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toolbar)
+
+
+    }
+}
