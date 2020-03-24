ea1ea121501b1cf3a3bb08c68667e3340248f56e
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2020-03-24 12:48:04 -0400

    implemetada guia 7

--- app/src/main/java/com/example/gua6_10/MainActivity.kt
+++ app/src/main/java/com/example/gua6_10/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.gua6_10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.example.gua6_10.toolbar as toolbar1
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+
+            R.id.action_search ->{
+                Log.d("TEST", "ICON SEARCH")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
